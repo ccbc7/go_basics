a2ee0f6341bcab4ef6ac9beaa21e1a5842d67aac
ccbc7 <flexibleelbow@gmail.com>
2024-09-02 16:49:19 +0900

    チャネルを閉じる

--- backend/main.go
+++ backend/main.go
@@ -2,18 +2,73 @@ package main
 
 import (
 	"fmt"
-	"os"
-
-	"go_basics/calculator"
-
-	"github.com/joho/godotenv"
+	"sync"
+	"time"
 )
 
 func main() {
-	godotenv.Load()
-	fmt.Println(os.Getenv("ENV"))
+	ch1 := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(1)
 
-	fmt.Println(calculator.Offset)
-	fmt.Println("Sum: ", calculator.Sum(1, 2))
-	fmt.Println("Multiply: ", calculator.Multiply(1, 2))
+	go func() {
+		defer wg.Done()
+		fmt.Println(<-ch1)
+	}()
+
+	ch1 <- 10
+	close(ch1)
+
+	v, ok := <-ch1 // okにはch1のクローズ状態が入る
+	fmt.Printf("v: %v, ok: %v\n", v, ok)
+	wg.Wait()
+
+	// バッファ付きチャネルの場合, クローズがされても、読み込まれていないデータがあれば、okはtrueになる
+	ch2 := make(chan int, 2)
+	ch2 <- 20
+	ch2 <- 30
+	close(ch2)
+	v, ok = <-ch2
+	fmt.Printf("v: %v, ok: %v\n", v, ok) // 20, true
+	v, ok = <-ch2
+	fmt.Printf("v: %v, ok: %v\n", v, ok) // 30, true
+	v, ok = <-ch2
+	fmt.Printf("v: %v, ok: %v\n", v, ok) // 0, false
+
+	ch3 := generateCountStream()
+	for v := range ch3 {
+		fmt.Println(v)
+	}
+
+	// データの値を持たない通知専用のチャネル
+	nCH := make(chan struct{})
+	for i := 0; i <= 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			fmt.Printf("goroutine %v start\n", i)
+			<-nCH
+			fmt.Println(i)
+		}(i)
+	}
+	time.Sleep(2 * time.Second)
+	close(nCH)
+	fmt.Println("finish---")
+
+
+	wg.Wait()
+	fmt.Println("finish")
+}
+
+// カプセル化されたチャネルを生成する
+// 返り値として、読みとり専用のチャネルを返す
+func generateCountStream() <-chan int {
+	ch := make(chan int)
+	go func() {
+		defer close(ch)
+		for i := 0; i <= 5; i++ {
+			ch <- i
+		}
+	}()
+	return ch
 }
