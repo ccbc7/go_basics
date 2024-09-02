29cdb003e218c136b0bb1b674b08d1f9e1799a93
ccbc7 <flexibleelbow@gmail.com>
2024-09-02 18:10:12 +0900

    select default case

--- backend/main.go
+++ backend/main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"fmt"
-	"os"
-
-	"go_basics/calculator"
-
-	"github.com/joho/godotenv"
+	"sync"
+	"time"
 )
 
-func main() {
-	godotenv.Load()
-	fmt.Println(os.Getenv("ENV"))
+const bufSize = 3
 
-	fmt.Println(calculator.Offset)
-	fmt.Println("Sum: ", calculator.Sum(1, 2))
-	fmt.Println("Multiply: ", calculator.Multiply(1, 2))
+func main() {
+	var wg sync.WaitGroup
+	ch := make(chan string, bufSize)
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < bufSize; i++ {
+			time.Sleep(1000 * time.Millisecond)
+			ch <- "hello"
+		}
+	}()
+
+	// gorutineとは独立して、こちらのfor文が実行される
+	for i := 0; i < 3; i++ {
+		select {
+		case m := <-ch: // チャネルからメッセージを受信した場合
+			fmt.Println(m) // 受信したメッセージを表示
+		default: // メッセージが受信されなかった場合
+			fmt.Println("no message received") // メッセージが受信されなかったことを表示
+		}
+		time.Sleep(1500 * time.Millisecond) // 1.5秒間スリープ
+	}
+
+	wg.Wait() // ゴルーチンの完了を待つ
+	fmt.Println("Done")
 }
