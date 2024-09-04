f57db368ca4613eebd8d6a3218922e9633df6b60
ccbc7 <flexibleelbow@gmail.com>
2024-09-04 11:38:15 +0900

    pipline

--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,74 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"os"
-
-	"go_basics/calculator"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load()
-	fmt.Println(os.Getenv("ENV"))
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	fmt.Println(calculator.Offset)
-	fmt.Println("Sum: ", calculator.Sum(1, 2))
-	fmt.Println("Multiply: ", calculator.Multiply(1, 2))
+	nums := []int{1, 2, 3, 4, 5}
+	var i int
+
+	for v := range double(ctx, offset(ctx, double(ctx, generator(ctx, nums...)))) {
+		if i == 3 {
+			cancel()
+		}
+		fmt.Println(v)
+		i++
+	}
+
+	fmt.Println("finished")
+}
+
+/*
+* 第1引数: context.Context
+* 第2引数: ...int(可変長引数)
+* 戻り値: <-chan int(読み取り専用チャネル)
+*/
+func generator(ctx context.Context, nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for _, n := range nums {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- n:
+			}
+		}
+	}()
+	return out
+}
+
+func double(ctx context.Context, in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for n := range in {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- n * 2:
+			}
+		}
+	}()
+	return out
+}
+
+func offset(ctx context.Context, in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for n := range in {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- n + 2:
+			}
+		}
+	}()
+	return out
 }
